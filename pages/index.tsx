5f927f00d58d0579c0462e84c2c4fe48f38c3243
OH-JOO-YEONG <brb1111@naver.com>
2022-11-07 23:54:25 +0900

    [Feat] 핸들러 연결진행중

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,46 @@
+import axios from "axios";
+import { useCallback, useEffect, useRef } from "react";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const onUploadImage = useCallback(
+    async (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (!e.target.files) {
+        return;
+      }
+
+      try {
+        const formData = new FormData();
+        formData.append("file", e.target.files[0]);
+
+        const { data } = await axios.post("/api/data", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    []
+  );
+
+  const onUploadImageButtonClick = useCallback(() => {
+    if (!inputRef.current) {
+      return;
+    }
+    inputRef.current.click();
+  }, []);
+
+  useEffect(() => {}, []);
+
+  return (
+    <>
+      <input type="file" ref={inputRef} onChange={onUploadImage} />
+      <button onClick={onUploadImageButtonClick} />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
