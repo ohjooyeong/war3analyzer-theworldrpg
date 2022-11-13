f725897ea8d2a12a8e4ea19834c80e772ee035e4
OH-JOO-YEONG <brb1111@naver.com>
2022-11-13 22:37:27 +0900

    [Feat] 리플레이 파일 불러오기 성공

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -1,32 +1,46 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-const ReplayParser = require("w3gjs/dist/lib/parsers/ReplayParser").default;
+const W3GReplay = require("w3gjs").default;
+import multer from "multer";
+const parser = new W3GReplay();
 
-import fs from "fs";
-
-import middleware from "../../middleware/middleware";
 import nextConnect from "next-connect";
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+});
+
+const uploadMiddleware = upload.any();
 
 const handler = nextConnect();
 
-handler.use(middleware);
+handler.use(uploadMiddleware);
 
-handler.post(async (req, res) => {
+handler.post(async (req: any, res: any) => {
   try {
-    const files = req.files;
-    const body = req.body;
+    const file = req.files[0];
+    const gamedatablock = [];
+    let basic_info = {};
+    parser.on("basic_replay_information", (info) => (basic_info = info));
 
-    console.log(files, body);
+    parser.on("gamedatablock", (block) => {
+      gamedatablock.push(block);
+    });
+    const result = await parser.parse(file.buffer);
 
     // do stuff with files and body
-    res.status(200).json({});
+    res.status(200).json({ result, gamedatablock, basic_info });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 });
 
+export default handler;
+
 export const config = {
   api: {
     bodyParser: false, // Disallow body parsing, consume as stream
+    responseLimit: false,
   },
 };
